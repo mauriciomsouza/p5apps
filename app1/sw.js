40d2213650253f5b463781598832e18d36808da3
Mauricio Melo <souza.msmauricio@gmail.com>
2020-05-20 13:09:20 -0300

    Update sw.js

--- app1/sw.js
+++ app1/sw.js
@@ -11,3 +11,7 @@ self.addEventListener('install', e => {
     })
   );
 });
+
+this.addEventListener('fetch', function (event) {
+    // it can be empty if you just want to get rid of that error
+});
